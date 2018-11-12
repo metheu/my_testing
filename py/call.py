dc5410064cc35d2f8d39223f4781f793c86d0ed4
mbuhagiar <mbuhagiar@productsavvy.com>
2018-11-12 17:51:43 +0400

    added flow

--- py/call.py
+++ py/call.py
@@ -8,3 +8,9 @@ GOTIFY_password = os.getenv("gotify_password")
 r = requests.get(GOTIFY_URL)
 
 print(r.text)
+
+""" controller will do the following 
+    1. ping server and check its live else it will log error and retry n times
+    2. it will check if its app token or else it will request it """
+    
+    
